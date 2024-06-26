3b5dda42ac29aab0c2642a970d812599c2aa27ed
gexiao <xiaoge740@gmail.com>
2024-06-26 21:41:49 +0800

    完善第一个表的样式

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,10 @@
 <template>
-  <div id="app">
-    <total-sale></total-sale>
-    <today-user></today-user>
-    <total-order></total-order>
-   
-  </div>
 </template>
 
-<script>
-import TotalSale from '@/components/tables/totalSale.vue';
-import TotalOrder from '@/components/tables/totalOrder.vue';
-import TodayUser from '@/components/tables/todayUser.vue';
+<script setup>
 
 
-export default {
-  components: {
-    TodayUser,
-    TotalOrder,
-    TotalSale
-  }
-}
 </script>
 
-<style lang="scss" scoped>
-#app {
-  display: flex;
-  justify-content: space-between;
-  align-items: stretch;
-
-  > * {
-    flex: 1;
-    margin: 0 10px;
-    // border: 1px solid #ccc;
-    padding: 20px;
-    box-sizing: border-box;
-  }
-}
+<style scoped>
 </style>
--- src/components/tables/totalSale.vue
+++ src/components/tables/totalSale.vue
@@ -5,19 +5,19 @@
           <div class="comparison">
             <div class="comparison-item">
               <span class="light-text">日同比</span>
-              <span>2.33%</span>
+              <span class="percentage">2.33%</span>
               <span class="increase"></span>
             </div>
             <div class="comparison-item">
               <span class="light-text">月同比</span>
-              <span>4.14%</span>
+              <span class="percentage">4.14%</span>
               <span class="increase"></span>
             </div>
           </div>
         </template>
         <template #footer>
           <span>昨日销售额</span>
-          <span>￥200000</span>
+          <span class="bold-dark">￥200000</span>
         </template>
       </CommonCard>
     </div>
@@ -37,31 +37,42 @@
   .comparison {
     display: flex;
     flex-direction: column;
-    font-size: 1.2em; // 让 default 插槽内的文字字体大一点
-    display: flex;
+    font-size: 1.4em; // 让 default 插槽内的文字字体大一点
     justify-content: flex-start;
+    // background-color: pink;
+    margin-left: -165px;
   
     .comparison-item {
       display: flex;
       align-items: center;
       justify-content: flex-start; // 让日同比文字和月同比文字居中靠左对齐
-      margin:5px auto;
-    }
+      margin: 5px auto;
   
-    .light-text {
-      color: #999; // 调整颜色以使文字变淡
-    }
+      .light-text {
+        color: rgb(83, 83, 83); 
+      }
   
-    span.increase {
-      display: inline-block;
-      width: 0;
-      height: 0;
-      border-width: 4px;
-      border-style: solid;
-      border-color: transparent transparent green transparent;
-      margin-left: 10px;
-      transform: translateY(-50%);
+      .percentage {
+        margin-left: 6px; // 增加文字和百分比数字之间的空隙
+        color: rgb(50, 50, 50); // 让百分比数字的颜色更深一点
+      }
+  
+      span.increase {
+        display: inline-block;
+        width: 0;
+        height: 0;
+        border-width: 4px;
+        border-style: solid;
+        border-color: transparent transparent green transparent;
+        margin-left: 10px;
+        transform: translateY(-50%);
+      }
     }
+  }
+  
+  .bold-dark {
+    font-weight: bold; // 让字体更粗
+    color: rgb(50, 50, 50); // 让颜色更深
   }
   </style>
   
